f8e3160fd0e5f8150458b36e852cfd366df4eb8b
GIT-Gizmo <boluforjesus@gmail.com>
2024-08-08 09:53:11 +0100

    Completed the success page and the patient side of the app

--- app/patients/[userid]/new-appointment/success/page.tsx
+++ app/patients/[userid]/new-appointment/success/page.tsx
@@ -1,8 +1,16 @@
+import { Button } from '@/components/ui/button';
+import { Doctors } from '@/constants';
+import { getAppointment } from '@/lib/actions/appointment.actions';
+import { formatDateTime } from '@/lib/utils';
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-const Success = () => {
+const Success = async ({ params: { userId }, searchParams }: SearchParamProps) => {
+    const appointmentId = (searchParams?.appointmentId as string) || "";
+    const appointment = await getAppointment(appointmentId);
+    const doctor = Doctors.find((doc) => doc.name === appointment.primaryPhysician)
+
     return (
         <main className='flex h-screen max-h-screen px-[5%]'>
             <div className="success-img">
@@ -15,6 +23,48 @@ const Success = () => {
                         className="h-10 w-fit"
                     />
                 </Link>
+
+                <section className="flex flex-col items-center">
+                    <Image
+                        src="/assets/gifs/success.gif"
+                        width={280}
+                        height={300}
+                        alt='success'
+                    />
+                    <h2 className="header mb-6 text-center max-w-[600px]">
+                        Your <span className="text-green-500">appointment request</span> has been successfully submitted.
+                    </h2>
+                    <p>We will be in touch shortly to confirm.</p>
+                </section>
+
+                <section className="request-details">
+                    <p>Requested appointment details:</p>
+                    <div className="flex items-center gap-3">
+                        <Image
+                            src={doctor?.image!}
+                            width={100}
+                            height={100}
+                            alt='doctor image'
+                            className='size-6'
+                        />
+                        <p className="whitespace-nowrap">Dr. {doctor?.name}</p>
+                    </div>
+                    <div className="flex gap-2">
+                        <Image
+                            src="/assets/icons/calendar.svg"
+                            width={24}
+                            height={24}
+                            alt='calendar'
+                        />
+                        <p>{formatDateTime(appointment.schedule).dateTime}</p>
+                    </div>
+                </section>
+
+                <Button variant="outline" className="shad-primary-btn" asChild>
+                    <Link href={`/patients/${userId}/new-appointment`}>
+                        New Appointment</Link>
+                </Button>
+                <p className="copyright">© 2024 DocThud</p>
             </div>
         </main>
     )
